b52432dccf3bfb0cbb238d1ed1c44abf94067af9
Shahmatov Arseniy <62886550+cene555@users.noreply.github.com>
2022-05-24 23:04:56 +0300

    1

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name="imagen_pytorch",
+    packages=[
+        "imagen_pytorch",
+        "imagen_pytorch.clip",
+        "imagen_pytorch.tokenizer",
+    ],
+    package_data={
+        "imagen_pytorch.tokenizer": [
+            "bpe_simple_vocab_16e6.txt.gz",
+            "encoder.json.gz",
+            "vocab.bpe.gz",
+        ],
+        "imagen_pytorch.clip": ["config.yaml"],
+    },
+    install_requires=[
+        "Pillow",
+        "attrs",
+        "torch",
+        "filelock",
+        "requests",
+        "tqdm",
+        "ftfy",
+        "regex",
+        "numpy",
+        "blobfile",
+        "accelerate",
+	"transformers"
+    ],
+    author="",
+)
